1b1d2be1f5b6583f9444f58be64e7b5440782038
Ryne <rynechaloux@gmail.com>
2014-05-03 12:31:03 -0700

    Basic Mongo/Mongoose stuff added yay

--- packages/settings/public/controllers/settings.js
+++ packages/settings/public/controllers/settings.js
@@ -1,71 +1,66 @@
 'use strict';
 
-angular.module('mean').controller('SettingsController', ['$scope', 'Global',
-  function($scope, Global, Settings) {
+angular.module('mean').controller('SettingsController', ['$scope', '$http', 'Global',
+  function($scope, $http, Global, Settings) {
     $scope.global = Global;
     $scope.settings = {name:'zsettingss'};
 
-    $scope.drinks = [
-    {
-      title: "Liquor? I barely know her.",
-      bebidas: [{
-        name: 'Whiskey',
-        fullness: 50,
-        carbonated: false
-      }, {
-        name: 'Mezcal',
-        fullness: 40,
-        carbonated: false
-      }, {
-        name: 'Rum',
-        fullness: 10,
-        carbonated: true 
-      }, {
-        name: 'Vodka',
-        fullness: 78,
-        carbonated: false
-      }, {
-        name: 'Tequila',
-        fullness: 2,
-        carbonated: true
-      }, {
-        name: 'Gin',
-        fullness: 23,
-        carbonated: false
-      }]
-    },
-    {
-      title: "Mixer? I, uh, I got nothing.",
-      bebidas: [
-      {
-        name: 'Orange Juice',
-        fullness: 12,
-        carbonated: false
-      }, {
-        name: '7-Up',
-        fullness: 75,
-        carbonated: true
-      }, {
-        name: 'Cream',
-        fullness: 10,
-        carbonated: false
-      }, {
-        name: 'Lemonaid',
-        fullness: 28,
-        carbonated: false
-      }, {
-        name: 'Ice-T',
-        fullness: 69,
-        carbonated: false
-      }, {
-        name: 'Coke',
-        fullness: 23,
-        carbonated: true
-      }]
-    }
-    ];
+    // when landing on the page, get all drinks and show them
+    $scope.installedDrinks = [];
+    $scope.warmDrinks = [];
+    $scope.coldDrinks = [];
+    $scope.installedDrinks.push($scope.warmDrinks);
+    $scope.installedDrinks.push($scope.coldDrinks);
 
-    $scope.drinkBackup = $scope.drinks;
+    //Get all the currently installed drinks from db
+    console.log('Getting installedDrinks from db..');
+    $http.get('/api/installedDrinks')
+      .success(function(data) {
+        for(var i = 0; i < data.length; i++){
+          console.log('Drink ' + i + ': ' + data[i]);
+          if(i < 6){
+              $scope.warmDrinks.push(data[i]);
+          }
+          else{
+              $scope.coldDrinks.push(data[i]);
+          }
+        }
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+
+    $scope.updateDrink = function(solenoidIndex) {
+      if(solenoidIndex<=5){
+          $http.put('/api/installedDrinks/'+solenoidIndex, {
+          name:$scope.warmDrinks[solenoidIndex].name,
+          fullness: $scope.warmDrinks[solenoidIndex].fullness,
+          carbonated: $scope.warmDrinks[solenoidIndex].carbonated
+          })
+          .success(function(data) {
+            console.log('success!?');
+          })
+          .error(function(data) {
+            console.log('Error: ' + data);
+          });
+      }
+      else{
+          $http.put('/api/installedDrinks/'+solenoidIndex, {
+          name:$scope.coldDrinks[solenoidIndex%6].name,
+          fullness: $scope.coldDrinks[solenoidIndex%6].fullness,
+          carbonated: $scope.coldDrinks[solenoidIndex%6].carbonated
+          })
+          .success(function(data) {
+            console.log('success!?');
+          })
+          .error(function(data) {
+            console.log('Error: ' + data);
+          });
+      }
+      
+    };
+
+    $scope.drinkBackup = $scope.installedDrinks;
 
     $scope.suggestions = ["vodka","tequila","whiskey","kahlua","coke", "brandy", "wine"]
 
